548aacf2ada6d1feb3d309666996b05991bd6334
ret7020 <ret7020@gmail.com>
2022-12-02 20:26:31 +0300

    Profile workflow

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,45 @@
 import React, { useState, useEffect } from "react";
-import { Alert } from 'antd';
+import { Card, Avatar, Col, Row, Button } from 'antd';
 import { getUser } from "../api";
 import API_URL from "../config";
 import { useNavigate } from "react-router-dom";
+import { Typography } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
+
+const { Title } = Typography;
+
 
 const Home = () => {
-  const [userFio, setUserFio] = useState(null);
   const navigate = useNavigate();
   useEffect(() => {
-    getUser(() => {console.log("Auth")}, () => {navigate("/login")}, API_URL);
-  })
+    getUser(() => { console.log("Auth") }, () => { navigate("/login") }, API_URL);
+  });
+
   return (
     <>
-      <h1>Ваш профиль</h1>
-      <p>
-        Test Data from API: {userFio}
-      </p>
-      <Alert message="Вас забанил Путинцев" type="error" />
+      <Card
+        title="Ваш профиль"
+        bordered={false}
+      >
+        <Row align="middle">
+          <Col>
+            <Avatar src="http://localhost:8080/storage/avatars/1_avatar.png" size={100} />
+          </Col>
+          <Col style={{ marginLeft: 20 }}>
+            <Title level={2}>Степан Жданов</Title>
+            <Row>
+              <Button type="primary" icon={<GithubOutlined />} style={{marginRight: 10}}>
+                @ret7020
+              </Button>
+              <Button type="primary" icon={<GithubOutlined />}>
+                @ret7020
+              </Button>
+            </Row>
+          </Col>
+
+        </Row>
+
+      </Card>
     </>
 
   )
