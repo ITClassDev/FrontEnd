c23268b5a0ff5a2e9bc6684f8f8f70920c5440d6
ret7020 <ret7020@gmail.com>
2022-12-21 22:36:21 +0300

    Admin page; user manage ideas

--- src/Components/AdminUsers.jsx
+++ src/Components/AdminUsers.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-import { Typography, Table } from "antd";
+import { Typography, Table, Button, Space, Input } from "antd";
+
+const { Search } = Input;
 
 const { Title } = Typography;
+
+const UserControllButtons = () => {
+  return (
+    <Space direction="horizontal">
+      <Button type="primary">Редактировать</Button>
+      <Button type="dashed" danger>
+        Удалить
+      </Button>
+      <Button type="primary" danger>
+        Забанить (ахахахах)
+      </Button>
+    </Space>
+  );
+};
 
 const AdminUsers = () => {
   const allUsersColumns = [
@@ -16,22 +32,29 @@ const AdminUsers = () => {
       key: "fio",
     },
     {
-      title: "Редактировать",
-      dataIndex: "editBtn",
-      key: "editBtn",
+      title: "Действия",
+      dataIndex: "actionsBtns",
+      key: "actionsBtns",
     },
   ];
   const testUsersData = [
     {
       id: 1,
       fio: <>Stephan Zhdanov</>,
+      actionsBtns: <UserControllButtons />,
       key: 1,
     },
   ];
   return (
     <>
       <Title level={4}>Все пользователи</Title>
-      <Table columns={allUsersColumns} dataSource={testUsersData}/>
+      <Search
+        placeholder="Поиск пользователя по имени и фамилии"
+        enterButton
+        style={{ marginBottom: 20 }}
+      />
+
+      <Table columns={allUsersColumns} dataSource={testUsersData} />
     </>
   );
 };
