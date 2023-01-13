6b59869581cc710ee42371c65770c36a64754267
ret7020 <ret7020@gmail.com>
2023-01-14 01:54:42 +0300

    Minor code editor fixes

--- src/Components/SendTask.jsx
+++ src/Components/SendTask.jsx
@@ -9,7 +9,7 @@ const { Dragger } = Upload;
 const SendTask = ({ task_id, getSubmissions }) => {
   const [fileToSend, SetFileToSend] = useState();
   const [messageApi, contextHolder] = message.useMessage();
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
+  const [code, setCode] = useState(``);
 
   const props = {
     name: "file",
@@ -81,18 +81,20 @@ const SendTask = ({ task_id, getSubmissions }) => {
       label: "Набрать здесь",
       children: (
         <>
+        LANG SELECTOR
           <CodeEditor
             value={code}
             language="python"
-            placeholder="Please enter JS code."
+            placeholder="Введите код"
             onChange={(evn) => setCode(evn.target.value)}
             padding={15}
+            data-color-mode="dark"
             style={{
               fontSize: 12,
               backgroundColor: "#f5f5f5",
-              fontFamily:
-                "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+              fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
             }}
+
           />
           <Button type="primary" block style={{ marginTop: 20 }}>
             Отправить на проверку
