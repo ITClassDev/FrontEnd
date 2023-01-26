942d63ecf7aedf99a97333a13efe64f31ce195e0
ret7020 <ret7020@gmail.com>
2023-01-26 22:49:49 +0300

    Polls service

--- src/Pages/Poll.jsx
+++ src/Pages/Poll.jsx
@@ -9,16 +9,21 @@ import {
   Typography,
   Space,
   Checkbox,
+  Row,
+  Image
 } from "antd";
 import "../poll.css";
+import { useEffect } from "react";
 
 const { Text } = Typography;
+const { TextArea } = Input;
 
 const QuestionBase = ({ ind, question, body }) => {
   return (
-    <Card title="" style={{ marginTop: 40 }} key={ind}>
+    <Card title="" style={{ marginTop: 20 }} key={ind}>
       <Space direction="vertical" style={{ width: "100%" }}>
-        <Text strong>{question}</Text>
+        <Text strong>{question.text}</Text>
+        {"image" in question && <Image src={question.image} width={"40%"}/>}
         <Form.Item name={`qustion_${ind}`}>{body}</Form.Item>
       </Space>
     </Card>
@@ -32,7 +37,7 @@ const OneItemSelect = ({ ind, question }) => {
         <Select style={{ width: "100%" }} options={question.variants}></Select>
       }
       ind={ind}
-      question={question.text}
+      question={question}
     />
   );
 };
@@ -41,15 +46,47 @@ const OneLineText = ({ ind, question }) => {
   return (
     <QuestionBase
       body={
-        <Select style={{ width: "100%" }} options={question.variants}></Select>
+        <Input
+          style={{ width: "100%" }}
+          placeholder="Введите свой ответ"
+        ></Input>
       }
       ind={ind}
-      question={question.text}
+      question={question}
     />
   );
 };
 
-const MultilineText = ({ ind, question }) => {};
+const MultilineText = ({ ind, question }) => {
+  return (
+    <QuestionBase
+      body={
+        <TextArea
+          style={{ width: "100%" }}
+          placeholder="Введите свой ответ"
+        ></TextArea>
+      }
+      ind={ind}
+      question={question}
+    />
+  );
+};
+
+const CheckboxSelect = ({ ind, question }) => {
+  console.log(question);
+  return (
+    <QuestionBase
+      body={question.variants.map((el) => (
+        <Row>
+          <Checkbox value={el.value} />
+          <Text style={{ marginLeft: 5 }}>{el.label}</Text>
+        </Row>
+      ))}
+      ind={ind}
+      question={question}
+    />
+  );
+};
 
 const Poll = () => {
   // Handler
@@ -76,7 +113,39 @@ const Poll = () => {
         },
       ],
     },
+    {
+      text: "Кратко опишите Путинцева",
+      type: 1,
+    },
+    {
+      text: "Просто мультилайн",
+      type: 2,
+    },
+    {
+      text: "Checkbox select",
+      type: 3,
+      variants: [
+        { value: "0", label: "Option" },
+        { value: "1", label: "Option 1" },
+        { value: "2", label: "Option 2" },
+      ],
+    },
+    {
+      image:
+        "https://www.gentoo.org/assets/img/wallpaper/abducted/gentoo-abducted-1600x1200.png",
+      text: "With image support",
+      type: 1,
+    },
   ];
+  useEffect(() => {
+    console.log(String.raw`
+    ██████╗░██╗░░░██╗████████╗██╗███╗░░██╗░█████╗░███████╗██╗░░░██╗
+    ██╔══██╗██║░░░██║╚══██╔══╝██║████╗░██║██╔══██╗██╔════╝██║░░░██║
+    ██████╔╝██║░░░██║░░░██║░░░██║██╔██╗██║██║░░╚═╝█████╗░░╚██╗░██╔╝
+    ██╔═══╝░██║░░░██║░░░██║░░░██║██║╚████║██║░░██╗██╔══╝░░░╚████╔╝░
+    ██║░░░░░╚██████╔╝░░░██║░░░██║██║░╚███║╚█████╔╝███████╗░░╚██╔╝░░
+    ╚═╝░░░░░░╚═════╝░░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚════╝░╚══════╝░░░╚═╝░░░`);
+  }, []);
   return (
     <Layout>
       <div className="wrapper">
@@ -87,10 +156,27 @@ const Poll = () => {
           requiredMark={false}
           onFinish={formSubmitHandler}
         >
-          {questions.map((question, ind) => (
-            <OneItemSelect ind={ind} question={question} />
-          ))}
-          <Card style={{ marginTop: 20 }}>
+          {questions.map((question, ind) => {
+            let res;
+            switch (question.type) {
+              case 0:
+                res = <OneItemSelect ind={ind} question={question} key={ind} />;
+                break;
+              case 1:
+                res = <OneLineText ind={ind} question={question} key={ind} />;
+                break;
+              case 2:
+                res = <MultilineText ind={ind} question={question} key={ind} />;
+                break;
+              case 3:
+                res = (
+                  <CheckboxSelect ind={ind} question={question} key={ind} />
+                );
+                break;
+            }
+            return res;
+          })}
+          <Card style={{ marginTop: 20, marginBottom: 20 }}>
             <Space direction="vertical" style={{ width: "100%" }}>
               <Checkbox>
                 Отправляя опрос, Вы отдаёте себя в рабство Путинцеву навсегда
@@ -104,6 +190,7 @@ const Poll = () => {
               </Button>
             </Space>
           </Card>
+          <Text italic >Polls - Open Source ShTP Service</Text>
         </Form>
       </div>
     </Layout>
