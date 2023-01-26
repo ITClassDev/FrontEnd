382ef5c370d85fa23e2556351f90fc27fad2bae4
ret7020 <ret7020@gmail.com>
2023-01-26 20:04:57 +0300

    Polls worflow

--- src/Pages/Contest.jsx
+++ src/Pages/Contest.jsx
@@ -42,7 +42,6 @@ const Contest = () => {
     }
   }
 
-  
   useEffect(() => {
     getContestData(
       contest_id,
--- src/Pages/Poll.jsx
+++ src/Pages/Poll.jsx
@@ -14,6 +14,43 @@ import "../poll.css";
 
 const { Text } = Typography;
 
+const QuestionBase = ({ ind, question, body }) => {
+  return (
+    <Card title="" style={{ marginTop: 40 }} key={ind}>
+      <Space direction="vertical" style={{ width: "100%" }}>
+        <Text strong>{question}</Text>
+        <Form.Item name={`qustion_${ind}`}>{body}</Form.Item>
+      </Space>
+    </Card>
+  );
+};
+
+const OneItemSelect = ({ ind, question }) => {
+  return (
+    <QuestionBase
+      body={
+        <Select style={{ width: "100%" }} options={question.variants}></Select>
+      }
+      ind={ind}
+      question={question.text}
+    />
+  );
+};
+
+const OneLineText = ({ ind, question }) => {
+  return (
+    <QuestionBase
+      body={
+        <Select style={{ width: "100%" }} options={question.variants}></Select>
+      }
+      ind={ind}
+      question={question.text}
+    />
+  );
+};
+
+const MultilineText = ({ ind, question }) => {};
+
 const Poll = () => {
   const formSubmitHandler = (form_data) => {
     console.log(form_data);
@@ -42,28 +79,27 @@ const Poll = () => {
     <Layout>
       <div className="wrapper">
         <Card title="Опрос от Путинцева">Этот опрос составил Путинцев</Card>
-        <Form name="answers_page" autoComplete="off" requiredMark={false} onFinish={formSubmitHandler}>
+        <Form
+          name="answers_page"
+          autoComplete="off"
+          requiredMark={false}
+          onFinish={formSubmitHandler}
+        >
           {questions.map((question, ind) => (
-            <Card title="" style={{ marginTop: 40 }} key={ind}>
-              <Space direction="vertical" style={{ width: "100%" }}>
-                <Text strong>{question.text}</Text>
-                <Form.Item
-                  name="putincev"
-                >
-                  <Select
-                    style={{ width: "100%" }}
-                    options={question.variants}
-                  ></Select>
-                </Form.Item>
-              </Space>
-            </Card>
+            <OneItemSelect ind={ind} question={question} />
           ))}
           <Card style={{ marginTop: 20 }}>
             <Space direction="vertical" style={{ width: "100%" }}>
               <Checkbox>
                 Отправляя опрос, Вы отдаёте себя в рабство Путинцеву навсегда
               </Checkbox>
-              <Button type="primary" htmlType="submit">Отправить</Button>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ marginTop: 15 }}
+              >
+                Отправить
+              </Button>
             </Space>
           </Card>
         </Form>
