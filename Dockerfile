a39733e5ccbb7e0ec8d73a2bc040cc3de3e21142
ret7020 <ret7020@gmail.com>
2023-04-17 14:08:20 +0000

    pm2 works

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,8 @@ RUN npm run build
 ENV NODE_ENV production
 EXPOSE 3000
 #CMD [ "npx", "serve", "build" ]
+#CMD ["npx", "pm2", "serve", "build", "--spa", "--port", "3000"]
+#CMD ["sleep", "infinity"]
+#CMD ["tail", "-f", "/dev/null"]
+#CMD ["npx", "pm2", "--no-daemon", "serve", "build", "--port", "3000"]
+
--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:8080";
-export const STORAGE = "http://localhost:8080/storage";
-export const FRONTEND_URL = "http://localhost:3000";
+export const API_URL = "http://192.168.115.200:8080";
+export const STORAGE = "http://192.168.115.200:8080/storage";
+export const FRONTEND_URL = "http://192.168.115.200:3000";
 export const CLIENT_VER = "0.0.3 dev";
 export default API_URL;
